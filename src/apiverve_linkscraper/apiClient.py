7e49506cedb5092523bc50f27d5198427c30db92
APIVerve Team <hello@apiverve.com>
2025-03-28 17:30:35 -0500

    Bug Fixes

--- src/apiverve_linkscraper/apiClient.py
+++ src/apiverve_linkscraper/apiClient.py
@@ -10,10 +10,10 @@ class LinkscraperAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/linkscraper'
+        self.base_url = 'https://api.apiverve.com/v1/linkscraper'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
